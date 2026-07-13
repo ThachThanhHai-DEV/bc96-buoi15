f154df9d8812a26aeb61e14be74ba5ff84bc6841
ThachThanhHaiDEV <haithachkstc02@gmail.com>
2026-07-13 21:28:34 +0700

    Hàm funtion

--- js/funtion.js
+++ js/funtion.js
@@ -49,3 +49,40 @@ xinChao(75.5)
 xinChao(7.0)
 xinChao(true)
 
+function dienTichHCN(a ,b){
+    console.log('Diện tích HCN với chiều độ dài các cạnh lần lượt là ${a}, ${b} là: ', a*b)
+}
+
+dienTichHCN(3 ,5)
+
+function dtb(a, b, c){
+    let diem = ( a+b+c)/3;
+    console.log('Điểm TB là :' ,diem.toFixed(2))
+    return diem // trả về 1 gtri, ngưng hàm. 
+    // nếu gọi return thì phải có thằng hứng giá trị
+}
+
+function xeploai(diemTB){
+    let kq =""
+    if (diemTB >=8){
+        kq ="HSG"
+    } else if (diemTB >=6.5){
+        kq = "HSK"
+    } else if (diemTB >=5){
+        kq ="HSTB"
+    } else { kq = "HSY"}
+  console.log( "XEP LOAI :", kq)
+}
+
+a=8;
+b=9;
+c=10;
+ xeploai(dtb(a,b,c));
+
+ function tong(a,b){
+    return a+b;
+ }
+ let res = tong(10,30);
+ console.log("Tong la: ", res)
+ 
+
